8863880d460d3d6d1798429770d4f7d8d7398370
shibliRz <shiblirz92@gmail.com>
2023-06-07 10:11:21 +0530

    Merge conflict fixed

--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,12 @@ Route::get('/doctor/book-an-appointment', function () {
 Route::get('/about',[AboutUsController::class,'index'])->name('about');
 Route::get('/contact',[ContactUsController::class,'index'])->name('contact');
 Route::get('/centers-of-excellence/{slug}',[CoeController::class,'index'])->name('coe');
-<<<<<<< HEAD
 Route::post('/',[FormController::class,'homeForm'])->name('home.form');
 Route::post('/centers-of-excellence',[FormController::class,'talkToDoctor'])->name('coe.form');
 Route::post('/contact',[FormController::class,'contactForm'])->name('contact.form');
 Route::get('/condition-treatments', function(){
     return view('condition-treatments');
 });
-=======
 Route::get('/locations/{slug}',[BranchController::class,'index'])->name('branch');
 Route::get('/international-patient',[InternationalPatientController::class,'index'])->name('international_patient');
 Route::get('/career',[CareerController::class,'index'])->name('career');
->>>>>>> eb74735507bfda92571d78e259a8f962905a9692
