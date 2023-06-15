675c651165607dd94572ad6ed7b4b5dd53b9ad50
Veeru Kumar <veerukumarv1@gmail.com>
2023-06-15 11:09:12 +0530

    Bug fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             PermissionSeeder::class,
             AdminSeeder::class,
+            UserSeeder::class,
             AssignPermissionSeeder::class,
-            UserSeeder::class
         ]);
     }
 }
