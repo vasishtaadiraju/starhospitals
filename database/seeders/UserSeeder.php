a69448f49791ab2e56e713a2ade28e0762c3ffde
Veeru Kumar <veerukumarv1@gmail.com>
2023-06-15 10:39:13 +0530

    Bug fixed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,70 +14,94 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert(
-            [
-                'name' => 'Swapna',
-                'email' => 'swapna@denary.agency',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Sakshee',
-                'email' => 'sakshee@denary.agency',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Vasu',
-                'email' => 'bala.v@denary.agency',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Vinay',
-                'email' => 'vinay.p@denary.agency',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Dia',
-                'email' => 'diamehta.mayo@gmail.com',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Moise',
-                'email' => 'moiseofficial009@gmail.com',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Shayani',
-                'email' => 'shayani.de@starhospitals.co.in',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Naresh',
-                'email' => 'nareshreddy.c@starhospitals.co.in',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Ashok',
-                'email' => 'ashokvardhan.k@starhospitals.co.in',
-                'password' => Hash::make('StarHospitals@123'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-        );
+        DB::table('users')->insert([
+            'name' => 'Swapna',
+            'email' => 'swapna@denary.agency',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 2,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Sakshee',
+            'email' => 'sakshee@denary.agency',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 3,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Vasu',
+            'email' => 'bala.v@denary.agency',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 4,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Vinay',
+            'email' => 'vinay.p@denary.agency',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 5,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Dia',
+            'email' => 'diamehta.mayo@gmail.com',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 6,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Moise',
+            'email' => 'moiseofficial009@gmail.com',
+            'password' => Hash::make('StarHospitals@123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 7,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
