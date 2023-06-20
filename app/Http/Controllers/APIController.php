a27d74ab475b48550113a3b1a43fcc69fc92c0dc
Veeru Kumar <veerukumarv1@gmail.com>
2023-06-20 11:56:10 +0530

    Added user

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class APIController extends Controller
+{
+    public function createUser() {
+        DB::table('users')->insert([
+            'name' => 'Vasishta',
+            'email' => 'vasishta@outshade.com',
+            'password' => Hash::make('Outshade@007'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 11,
+            'role_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
