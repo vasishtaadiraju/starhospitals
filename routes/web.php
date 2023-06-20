9b61307d1d5339cfa7924e5fa5ee91110564242f
Veeru Kumar <veerukumarv1@gmail.com>
2023-06-20 11:26:09 +0530

    Added route for all forms

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,11 @@ Route::get('/career',[CareerController::class,'index'])->name('career');
 Route::get('/doctors/{branch_slug}/{speciality_slug}/{slug}',[DoctorController::class,'index'])->name('doctor');
 Route::get('/doctor/book-a-video-consultation/{slug}',[DoctorController::class,'videoConsultation'])->name('doctor.book-video-consultation');
 
-
+//Form routes
+Route::controller(FormController::class)->group(function () {
+    Route::post('/request-callback', 'request_callback');
+    Route::post('/talkToDoctor', 'talkToDoctor');
+    Route::post('/contactForm', 'contactForm');
+    Route::post('/internationalPatientEnquiryForm', 'internationalPatientEnquiryForm');
+    Route::post('/videoConsultation', 'videoConsultation');
+});
