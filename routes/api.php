fc49e84f2227bf188aa08c6e4bbd7a28439c2975
Veeru Kumar <veerukumarv1@gmail.com>
2023-06-20 11:59:48 +0530

    Added user

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/createUser', [APIController::class, 'createUser']);
+// Route::get('/createUser', [APIController::class, 'createUser']);
