968d13e514fb4e5e5817479fe6816be81882a3c5
shibliRz <shiblirz92@gmail.com>
2023-08-26 16:59:43 +0530

    Fixed Ui bugs

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -2,6 +2,7 @@
     <h2 class="heading__title section-heading">{{$title}}</h2>
     @if ($description != '' || $description != null)
     <p class="heading__sub-title">{{$description}}</p>
-        
+    @else
+    <div style="min-height: 2.5em"></div>
     @endif
 </div>
