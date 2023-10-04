f42e8ba7ced1092d229b9db748674b37a75d7535
shibliRz <shiblirz92@gmail.com>
2023-10-04 12:35:20 +0530

    Added GTM code

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -17,6 +17,15 @@
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
     
     @yield('css')
+    
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T5VW8HQ');</script>
+    <!-- End Google Tag Manager -->
+    
     @stack('page-schema')
     {{-- <style>
         @font-face {
@@ -27,7 +36,10 @@
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5VW8HQ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 {{-- <x-patient-enquiry/> --}}
     
     <x-header :$coes :$branches :$specialists/> 
