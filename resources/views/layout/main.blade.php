a662f2ee1cfdedc6bf0656031826f3d10a4da06b
shibliRz <shiblirz92@gmail.com>
2023-10-06 12:00:04 +0530

    Updated G tag

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,24 +8,31 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('meta-description', ' ')">
     <title>@yield('meta-title', 'Star Hospitals')</title>
-    <link rel="icon" href="https://financial-district.starhospitals.in/wp-content/uploads/2023/02/cropped-favicon-32x32.png" sizes="32x32">
+    <link rel="icon"
+        href="https://financial-district.starhospitals.in/wp-content/uploads/2023/02/cropped-favicon-32x32.png"
+        sizes="32x32">
     {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
         rel="stylesheet"> --}}
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
-    
+
     @yield('css')
-    
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-T5VW8HQ');</script>
-    <!-- End Google Tag Manager -->
-    
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-585586121"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-585586121');
+    </script>
+
     @stack('page-schema')
     {{-- <style>
         @font-face {
@@ -36,17 +43,18 @@
 </head>
 
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5VW8HQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5VW8HQ" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-{{-- <x-patient-enquiry/> --}}
-    
-    <x-header :$coes :$branches :$specialists/> 
-    @yield('content')
-    <x-footer :$coes :$branches/>
+    {{-- <x-patient-enquiry/> --}}
 
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <x-header :$coes :$branches :$specialists />
+    @yield('content')
+    <x-footer :$coes :$branches />
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
     @yield('js')
 </body>
