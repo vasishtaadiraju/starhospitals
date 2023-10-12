3cc965874ab9e6a56b979b26cb2a58a480be1564
shibliRz <shiblirz92@gmail.com>
2023-10-12 11:03:40 +0530

    Created health package landing page

---
+++ public/lp/health-packages/thank-you.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Thank You</title>
+<!-- Global site tag (gtag.js) - Google Ads: 877214935 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-877214935"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-877214935'); </script> 
+	
+	<!-- Event snippet for Contact conversion page --> <script> gtag('event', 'conversion', {'send_to': 'AW-877214935/MsSYCNiP8ucBENf5pKID'}); </script> 
+	 <!--<meta http-equiv="refresh" content="5;url=https://www.apollocradle.com/landingpage/pediatric/thank-you.php" /> -->
+
+</head>
+
+<body style="background: #00799e33;">
+<div style=" text-align: center; padding: 200px; font-family:Arial; font-size: 22px;">
+	 <h4 class="thanku">Thank you for contacting us.</h4>
+	<p>We have received your enquiry and We'll get back to you very soon.</p></div>
+</body>
+</html>
