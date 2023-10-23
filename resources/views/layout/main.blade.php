26e1967f4c8ed3056096c0f607b62171ecb40bc5
shibliRz <shiblirz92@gmail.com>
2023-10-23 17:42:23 +0530

    Added GTM code and fixed robots.txt

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -19,7 +19,13 @@
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
 
     @yield('css')
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T5VW8HQ');</script>
+    
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-585586121"></script>
     <script>
@@ -49,7 +55,6 @@
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     {{-- <x-patient-enquiry/> --}}
-
     <x-header :$coes :$branches :$specialists />
     @yield('content')
     <x-footer :$coes :$branches />
