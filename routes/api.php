2f58e8ce331cc41c50d6ae6ac6dfe8b81215e7f1
Veeru Kumar <veerukumarv1@gmail.com>
2023-11-18 13:50:09 +0530

    Create User

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/createUser', [APIController::class, 'createUser']);
