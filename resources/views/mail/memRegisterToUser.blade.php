b0c598302b9b8308a623d984123a83263ba0a81f
shibliRz <shiblirz92@gmail.com>
2024-02-19 13:48:44 +0530

    Update: MEM pages

--- resources/views/mail/memRegisterToUser.blade.php
+++ resources/views/mail/memRegisterToUser.blade.php
@@ -13,7 +13,7 @@
     We wanted to take a moment to express our gratitude for choosing Star Hospitals for your upcoming course. At Star Hospitals, we are committed to providing exceptional learning experiences and equipping our students with the knowledge and skills needed to excel in the healthcare industry.
     As the next step, we kindly request you to update the necessary details in the attached document and send it back to us. Upon receiving the complete information, we will proceed with your application.
     <br><br>
-    Should you have any queries or require assistance, please don't hesitate to reach out to our team. We're here to ensure that your experience with Star Hospitals exceeds your expectations. We're looking forward to embarking on this educational journey together.
+   <b> Should you have any queries or require assistance, please don't hesitate to reach out to our team. We're here to ensure that your experience with Star Hospitals exceeds your expectations. We're looking forward to embarking on this educational journey together.</b>
     <br><br>
     Regards,
     Star Hospitals
