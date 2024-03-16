a7bd72c1d6221793289c2b7894bc84fb1e037ca4
jyoti963 <115809142+jyoti963@users.noreply.github.com>
2024-03-16 11:47:59 +0530

    whychooseus dynamic

--- resources/views/components/why-choose-us.blade.php
+++ resources/views/components/why-choose-us.blade.php
@@ -1,36 +1,41 @@
 <x-heading :title="'Why Choose Us'" :description="''" />
 <div class="choose-us ">
+    @if($content != null)
+        @if(isset($content->choose1_icon) && $content->choose1_icon != null)
         <div class="choose-us__card">
             <div class="choose-us__card__icon-wrapper">
-                <img  src="" data-src="{{Vite::asset('resources/images/Icons/medical 1.png')}}" alt="">
+                {{--  <img  src="" data-src="{{Vite::asset('resources/images/Icons/medical 1.png')}}" alt="">  --}}
+                {!!$content->choose1_icon !!}
             </div>
             <div>
-                <h3>Empathy</h3>
-                <p>Central to Star Hospitals' ethos is the
-                    understanding that patients are more than just medical cases—they are individuals with unique needs and concerns. The hospital fosters a culture of empathy among its staff, ensuring that patients feel heard, understood,
-                    and supported throughout their medical journey.</p>
+                <h3>{{ $content->choose1_title }}</h3>
+                <p>{{$content->choose1_description}}</p>
             </div>
         </div>
+        @endif
+        @if(isset($content->choose2_icon) && $content->choose2_icon != null)
         <div class="choose-us__card">
             <div class="choose-us__card__icon-wrapper">
-                <img  src="" data-src="{{Vite::asset('resources/images/Icons/Group.png')}}" alt="">
+                {{--  <img  src="" data-src="{{Vite::asset('resources/images/Icons/Group.png')}}" alt="">  --}}
+                {!!$content->choose2_icon !!}
             </div>
             <div>
-                <h3>Expertise</h3>
-                <p>Star Hospitals is committed to being the best by ensuring that its staff, from doctors to support personnel, are highly skilled and knowledgeable in their respective fields. This expertise allows them to provide top-notch medical care and innovative treatments to their
-                    patients.</p>
+                <h3>{{ $content->choose2_title }}</h3>
+                <p>{{$content->choose2_description}}</p>
             </div>
         </div>
+        @endif
+        @if(isset($content->choose3_icon) && $content->choose3_icon != null)
         <div class="choose-us__card">
             <div class="choose-us__card__icon-wrapper">
-                <img  src="" data-src="{{Vite::asset('resources/images/Icons/Group 2.png')}}" alt="">
+                {{--  <img  src="" data-src="{{Vite::asset('resources/images/Icons/Group 2.png')}}" alt="">  --}}
+                {!!$content->choose3_icon !!}
             </div>
             <div>
-                <h3>Excellence</h3>
-                <p>The hospital strives for excellence in all aspects of its operations, including patient care, medical outcomes, and
-                    customer service. They continuously seek to improve their practices and processes to deliver the highest quality of care possible.</p>
+                <h3>{{ $content->choose3_title }}</h3>
+                <p>{{$content->choose3_description}}</p>
             </div>
         </div>
-
-
+        @endif
+    @endif
 </div>
